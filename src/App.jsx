74c5c32d4393ed701dacb769ea15d8c02eb85027
jitesh <jitesh.kumar05official@gmail.com>
2024-07-03 21:54:43 +0530

    trying to resolve error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { FaEdit } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
