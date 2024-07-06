bdb980df7d2ab85785a758662a4f3bdb009227b3
jitesh <jitesh.kumar05official@gmail.com>
2024-07-06 15:39:19 +0530

    trying to fix the error

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <>
-    <nav className="flex justify-around bg-slate-700 text-white py-2 ">
-        <div className="logo">
-            <span className=' font-bold lg:text-xl md:text-lg  '>JK's Planner</span>
-        </div>
-        <ul className='flex gap-8 mx-10 '>
-            <li className=' hover:font-bold transition-all duration-500 '><a href="#">Home</a></li>
-            <li className=' hover:font-bold transition-all duration-500 '><a href="#">About</a></li>
-            <li className=' hover:font-bold transition-all duration-500 '><a href="#">Contact</a></li>
-        </ul>
+    <nav className="flex justify-around bg-slate-700 text-white py-2">
+      <div className="logo">
+        <span className='font-bold lg:text-xl md:text-lg'>JK's Planner</span>
+      </div>
+      <ul className='flex gap-8 mx-10'>
+        <li className='hover:font-bold transition-all duration-500'><a href="#">Home</a></li>
+        <li className='hover:font-bold transition-all duration-500'><a href="#">About</a></li>
+        <li className='hover:font-bold transition-all duration-500'><a href="#">Contact</a></li>
+      </ul>
     </nav>
-    </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
